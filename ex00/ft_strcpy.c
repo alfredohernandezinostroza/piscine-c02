3734b48ebc8be2fbfde63c4a96921fedbe813ae5
Alfredo Antonio Hernandez Inostroza <aantonio@c1r18s5.42lisboa.com>
2022-08-10 17:47:28 +0100

    finished ex00

--- ex00/ft_strcpy.c
+++ ex00/ft_strcpy.c
@@ -6,12 +6,20 @@
 /*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 17:25:53 by aantonio          #+#    #+#             */
-/*   Updated: 2022/08/10 17:26:25 by aantonio         ###   ########.fr       */
+/*   Updated: 2022/08/10 17:46:16 by aantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char    *ft_strcpy(char *dest, char *src)
+char	*ft_strcpy(char *dest, char *src)
 {
-    return dest;
-}
+	int i;
 
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
