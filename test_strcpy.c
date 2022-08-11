6df7fe277f0a094e76d34fcfb263a82bbc882a83
Alfredo A. Hernandez I <alfredo.hernandez@ug.uchile.cl>
2022-08-11 16:10:44 +0200

    fixed ex01

--- test_strcpy.c
+++ test_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 17:34:32 by aantonio          #+#    #+#             */
-/*   Updated: 2022/08/11 12:56:39 by aantonio         ###   ########.fr       */
+/*   Updated: 2022/08/11 15:09:51 by aantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,19 @@ int	main(void)
     char *ptr = str2;
 
     printf("%s ---- %s\n", str1, str2);
-	strncpy(str2, str1, 5);
+	ft_strncpy(str2, str1, 4);
+    printf("%s ---- %s\n", str1, str2);
+    printf("\n");
+    printf("%s ---- %s\n", str1, str2);
+	ft_strncpy(str2, str1, 5);
+    printf("%s ---- %s\n", str1, str2);
+    printf("\n");
+    printf("%s ---- %s\n", str1, str2);
+	ft_strncpy(str2, str1, 6);
+    printf("%s ---- %s\n", str1, str2);
+    printf("\n");
+    printf("%s ---- %s\n", str1, str2);
+	ft_strncpy(str2, str1, 7);
     printf("%s ---- %s\n", str1, str2);
     int i = 0;
     while(i<100){
