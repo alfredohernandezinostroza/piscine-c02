ed461dc6ec0437f9cf0ac461cb1e631649ce045a
Alfredo A. Hernandez I <alfredo.hernandez@ug.uchile.cl>
2022-08-11 13:58:47 +0200

    finished ex01

--- test_strcpy.c
+++ test_strcpy.c
@@ -6,13 +6,14 @@
 /*   By: aantonio <aantonio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 17:34:32 by aantonio          #+#    #+#             */
-/*   Updated: 2022/08/10 17:46:03 by aantonio         ###   ########.fr       */
+/*   Updated: 2022/08/11 12:56:39 by aantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <string.h>
 
-char	*ft_strcpy(char*, char*);
+char	*ft_strncpy(char*, char*, unsigned int);
 
 int	main(void)
 {
@@ -21,7 +22,7 @@ int	main(void)
     char *ptr = str2;
 
     printf("%s ---- %s\n", str1, str2);
-	ft_strcpy(str2, str1);
+	strncpy(str2, str1, 5);
     printf("%s ---- %s\n", str1, str2);
     int i = 0;
     while(i<100){
