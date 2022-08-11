354e148abda6e8840076282a0a21766501377b1d
Alfredo A. Hernandez I <alfredo.hernandez@ug.uchile.cl>
2022-08-11 15:14:16 +0200

    finished ex06

---
+++ test_ex06.c
@@ -0,0 +1,14 @@
+
+#include <ctype.h>
+#include <stdio.h>
+
+int ft_str_is_printable(char *);
+
+int main(void)
+{
+   char c;
+   for(c = 1; c <= 126; ++c)
+   	if (ft_str_is_printable(&c)!= 0)
+             printf("CHARACTER: %c, CODE: %d\n", c, c);
+   return 0;
+}
